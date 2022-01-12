19632bf1a988ce751f4395c325139641556df575
Vlad Usatii <vladusatii@gmail.com>
2022-01-11 18:07:19 -0600

    added GET and DELETE kwargs

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # 2gen
 
 A general-purpose generator for keys. Designed for all cryptocurrencies supporting the Bitcoin format of keys and addresses.
+
+## Functions
+
+To enable functions:
+
+```
+chmod +x main.py
+```
+
+Functions:
+
+```
+./main.py GET # GET will get either a new key if the database is found, or will make a new key object
+./main.py DELETE # DELETE will delete a key if the database is found
+```
+
+## TODO
+
+- Add other functions
--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 from bit import PrivateKeyTestnet
 import dbm
 from pathlib import Path
+import sys, os
 
 """
 Arguments:
 
-[ blank spot ] -- program will infer whether you have created a WIF or not
+GET            -- program will infer whether you have created a WIF or not
 DELETE         -- delete WIF cache, delete key
 
 """
@@ -19,17 +20,39 @@ WIF (PrivateAddress): {WIF}
 Public address      : {pubadd}
 """.format(**args))
 
-keypath = Path('WIF.db')
-if keypath.is_file():
-	with dbm.open('WIF', 'r') as db:
-		WIF = db['wif_address'].decode('utf-8')
-		key = PrivateKeyTestnet(WIF)
-	printall(key)
-else:
-	key = PrivateKeyTestnet()
-	with dbm.open('WIF', 'c') as db:
-		WIF = key.to_wif()
-		db['wif_address'] = WIF
-	printall(key)
+
+
+
+def main(kwargs):
+	keypath = Path('WIF.db')
+
+	if kwargs == 'GET':
+		if keypath.is_file():
+			with dbm.open('WIF', 'r') as db:
+				WIF = db['wif_address'].decode('utf-8')
+				key = PrivateKeyTestnet(WIF)
+			printall(key)
+		else:
+			key = PrivateKeyTestnet()
+			with dbm.open('WIF', 'c') as db:
+				WIF = key.to_wif()
+				db['wif_address'] = WIF
+			printall(key)
+	elif kwargs == 'DELETE':
+		if keypath.is_file():
+			prompter = input("Are you sure you want to delete your key? You will lose access to your coins, unless your WIF is documented. [Y/N]")
+			if prompter == 'Y' or prompter == 'y':
+				os.remove('WIF.db')
+			elif prompter == 'N' or prompter == 'n':
+				print('Operation canceled.')
+	else:
+		print('Not a valid argument.')
+
+
+if __name__ == '__main__':
+	try:
+		main(sys.argv[1]) # sys.argv[1] is an argument
+	except Exception:
+		print('Not a valid argument.')
 
 
